c652d664920e60f42a21b2eeec8229ef9d66f27c
keplerbit-71 <brandonkass11@gmail.com>
2023-12-27 22:37:35 -0600

    incomplete changes to option menu

--- Classes/OptionMenu.py
+++ Classes/OptionMenu.py
@@ -137,101 +137,59 @@ class Optiontrade(Menu):
             percents.append(percentchange); alltexts.append(texts)
 
         self.allrenders,self.selected_option = drawLatterScroll(screen,player.options,self.allrenders,self.bar.value,self.getpoints,(xshift,yshift),self.selected_option,mousebuttons,DH,alltexts,percents)
-        # for i,option in enumerate([(option) for i,option in enumerate(player.options) if i >= self.bar.value and i < self.bar.value+5]):# draws the stock graph bar
-            # ioffset = i+self.bar.value
+    def refreshOptions(self,stocklist:list):
+        self.calloptions = []; self.putoptions = []
+        for i in range(5):
+            stock = stocklist[random.randint(0,len(stocklist)-1)]
+            strikeprice = random.randint(math.floor(stock.price*0.95),math.ceil(stock.price*1.2))
+            self.putoptions.append(Option(stock,strikeprice,random.randint(3,25),'put'))
+        for i in range(5):
+            stock = stocklist[random.randint(0,len(stocklist)-1)]
+            strikeprice = random.randint(math.floor(stock.price*0.9),math.ceil(stock.price*1.1))
+            self.calloptions.append(Option(stock,strikeprice,random.randint(3,25),'call'))
 
-            # percentchange = ((option.get_value() - option.ogvalue) / option.ogvalue) * 100
+    def draw_Available(self,screen,mousebuttons):
+        mousex, mousey = pygame.mouse.get_pos()
+        xshift = 15
+        yshift = 150    
+        if len(self.allrenders) < len(player.options):# if the player has more stocks than the renders
+            for i in range((len(player.options)-len(self.allrenders))+1):# add the amount of renders needed
+                self.allrenders.append({})
+        if self.selected_option == None and len(player.options) > 0:
+            self.selected_option = 0
+        self.bar.scroll(mousebuttons)# check for the scroll of the bar
+        self.bar.changemaxvalue(len(player.options) if len(player.options) > 0 else 1)# change the max value of the bar based on the amount of stocks the player has
+
+        barheight = 520//len(player.options) if len(player.options) > 0 else 1
+
+        self.bar.draw_bar(screen, [225, DY], [45, DY + (yshift*4) - 80], 'vertical', barwh=[43, barheight], shift=85, reversedscroll=True, text=False)
+
+        self.drawSelectedStock(screen, self.selected_option, mousebuttons, player)# draws the additional stock info
+
+        percents = []; alltexts = []
+        for i, option in enumerate(player.options[self.bar.value:self.bar.value+5]):
+            percentchange = ((option.get_value() - option.ogvalue) / option.ogvalue) * 100
             
-            # if percentchange > 0:
-            #     grcolor = (0, 200, 0); profittext = "Profit"
+            if percentchange > 0:
+                grcolor = (0, 200, 0); profittext = "Profit"
                 
-            # elif percentchange == 0:
-            #     grcolor = (200, 200, 200); profittext = ""
-            # else:
-            #     grcolor = (225, 0, 0); profittext = "Loss"
+            elif percentchange == 0:
+                grcolor = (200, 200, 200); profittext = ""
+            else:
+                grcolor = (225, 0, 0); profittext = "Loss"
 
-            # textinfo = [[(190, 190, 190), 45],[(190, 190, 190), 35],[grcolor, 35],[grcolor, 35],[grcolor, 35]]
+            textinfo = [[(190, 190, 190), 45],[(190, 190, 190), 35],[grcolor, 35],[grcolor, 35],[grcolor, 35]]
 
-            # texts = [f'{option.stockobj.name} {option.option_type}',
-            #          f'Original Value: ${limit_digits(option.ogvalue,15)}',
-            #          f'Value: ${limit_digits(option.get_value(),15)}',
-            #          f'{profittext}: ${limit_digits((option.get_value() - option.ogvalue),15)}',
-            #          f'Change %: {limit_digits(percentchange,15)}%'
-            #         ]
-        #     self.allrenders = reuserenders(self.allrenders, texts, textinfo, i)
+            texts = [f'{option.stockobj.name} {option.option_type}',
+                     f'Original Value: ${limit_digits(option.ogvalue,15)}',
+                     f'Value: ${limit_digits(option.get_value(),15)}',
+                     f'{profittext}: ${limit_digits((option.get_value() - option.ogvalue),15)}',
+                     f'Change %: {limit_digits(percentchange,15)}%'
+                    ]
+            self.allrenders = reuserenders(self.allrenders, texts, textinfo, i)
+            percents.append(percentchange); alltexts.append(texts)
 
-        #     twidth = self.allrenders[i][texts[0]].get_width() + 25
-        #     twidth2 = max(self.allrenders[i][texts[1]].get_width(), self.allrenders[i][texts[2]].get_width()) + 30
-        #     twidth3 = max(self.allrenders[i][texts[3]].get_width(), self.allrenders[i][texts[4]].get_width()) + 45
-            
-        #     # find the points for the polygons
-        #     points,points2,points3,totalpolyon = self.getpoints(twidth, twidth2, twidth3, (i * xshift), (i * yshift))
-
-        #     # check if the mouse is hovering over the polygon
-        #     hover = False
-        #     if point_in_polygon((mousex, mousey), totalpolyon):  # check if mouse is inside the polygon
-        #         hover = True
-        #         if mousebuttons == 1:
-        #             self.selected_option = ioffset
-
-        #     polycolor = (30, 30, 30) if not hover else (80, 80, 80)
-        #     # polycolor = (60, 60, 60) if self.selected_option == ioffset else polycolor
-
-        #     # ----------draw the polygons----------
-        #     gfxdraw.filled_polygon(screen, points, polycolor)  # draws the first polygon with the name of the stock
-        #     gfxdraw.filled_polygon(screen, points2, (polycolor))  # draws the second polygon with the price of the stock
-        #     gfxdraw.filled_polygon(screen, points3, (polycolor))  # draws the third polygon with the profit of the stock
-
-        #     # ----------Draw the text----------
-        #     screen.blit(self.allrenders[i][texts[0]], (320 + (i * xshift), 235 + (i * yshift)))  # display name of stock
-        #     screen.blit(self.allrenders[i][texts[1]], (330 + (i * xshift) + twidth, 210 + (i * yshift)))# display bought price of stock
-        #     screen.blit(self.allrenders[i][texts[2]], (345 + (i * xshift) + twidth, 265 + (i * yshift)))# display current price of stock
-        #     screen.blit(self.allrenders[i][texts[3]], (330 + twidth + twidth2 + (i * xshift), 210 + (i * yshift)))# display profit of stock
-        #     screen.blit(self.allrenders[i][texts[4]], (345 + twidth + twidth2 + (i * xshift), 265 + (i * yshift)))# display percent change of stock
-            
-        #     # top left, top right, bottom right, bottom left
-        #     bottom_polygon = [[totalpolyon[0][0]+12, totalpolyon[0][1] + DH - 15], 
-        #                         [totalpolyon[1][0], totalpolyon[1][1]], 
-        #                         [totalpolyon[2][0], totalpolyon[2][1]], 
-        #                         [totalpolyon[3][0], totalpolyon[3][1]],
-        #                         [totalpolyon[3][0]-15, totalpolyon[3][1]],
-        #                         [totalpolyon[3][0]-3, totalpolyon[3][1] + DH - 15],
-        #                       ]
-        #     if hover or self.selected_option == ioffset:
-        #         if percentchange > 0:bottomcolor = (0, 200, 0)
-        #         elif percentchange == 0:bottomcolor = (120, 120, 120)
-        #         else:bottomcolor = (200, 0, 0)
-        #     else:
-        #         if percentchange > 0: bottomcolor = (0, 80, 0)
-        #         elif percentchange == 0: bottomcolor = (110, 110, 110)
-        #         else: bottomcolor = (80, 0, 0)
-
-        #     pygame.draw.polygon(screen, bottomcolor, bottom_polygon)
-        #     outlinecolor = (0, 0, 0) if self.selected_option != ioffset else (180, 180, 180)
-        #     pygame.draw.polygon(screen, outlinecolor, points, 5)  # draw the outline of the polygon
-        #     pygame.draw.polygon(screen, outlinecolor, points2, 5)  # draw the outline of the second polygon
-        #     pygame.draw.polygon(screen, outlinecolor, points3, 5)  # draw the outline of the third polygon
-            
-        # emptyboxnum = 5-len([(stock) for i,stock in enumerate(player.options) if i >= self.bar.value and i < self.bar.value+5])
-        # for i in range(emptyboxnum):
-        #     ioffset = i+(5-emptyboxnum)
-        #     points,points2,points3,totalpolyon = self.getpoints(150,200,200,(ioffset*xshift),(ioffset*yshift))
-            
-        #     gfxdraw.filled_polygon(screen, points, (30,30,30))
-        #     gfxdraw.filled_polygon(screen, points2, (30,30,30))
-        #     gfxdraw.filled_polygon(screen, points3, (30,30,30))
-        #     pygame.draw.polygon(screen, (0,0,0), points, 5)
-        #     pygame.draw.polygon(screen, (0,0,0), points2, 5)
-        #     pygame.draw.polygon(screen, (0,0,0), points3, 5)
-        #     screen.blit(self.emptytext, (320 + (ioffset * xshift), 235 + (ioffset * yshift)))  # display name of stock
-
-        # values = [option.get_value() for option in player.options]
-        # names = set([option.stockobj.name for option in player.options])
-        # values = [[sum([v[0] for v in values if v[1] == name]), name] for name in names]
-
-        # self.renderedback,self.renderedpietexts = draw_pie_chart(screen, values, 150,(1050, 200),self.renderedback,self.renderedpietexts)
-    def draw_Available(self):
-        pass
+        self.allrenders,self.selected_option = drawLatterScroll(screen,player.options,self.allrenders,self.bar.value,self.getpoints,(xshift,yshift),self.selected_option,mousebuttons,DH,alltexts,percents)
 
     def draw_menu_content(self, screen: pygame.Surface, stocklist: list, mousebuttons: int, player):
         """Draws all of the things in the option menu"""
