929f961a23673c4e9df142b26baace51e17075d1
keplerbit-71 <brandonkass11@gmail.com>
2024-08-21 21:12:41 -0500

    added sidescroll, even though i didn't code any of it

---
+++ SideScroll.py
@@ -0,0 +1,39 @@
+import pygame
+from Defs import *
+
+pygame.init()
+screen = pygame.display.set_mode([900,900])
+pygame.display.set_caption("Pygame Shell")
+
+class SideScroll:
+    def __init__(self) -> None:
+        pass
+            
+lastfps = deque(maxlen=300)
+clock = pygame.time.Clock()
+while True:
+    screen.fill((60,60,60))
+    # pygame.event.pump()
+    # pygame.draw.circle(screen, (255,255,255), (450,450), 100)
+
+    errors.update(screen)
+    screen.blits((text,pos) for text,pos in zip(update_fps(clock,lastfps),[(850,0),(850,30),(850,60)]))
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            pygame.quit()
+            quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("Mouse button pressed",pygame.mouse.get_pos())
+            
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_j:
+            print("Pressed the J key")
+
+
+    clock.tick(60)
+
+
+
+
+
